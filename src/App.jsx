d2c1f7247c24ac724b0d2d4e50537aa7fc33987d
AbdelhadiBelkhadim <belkhdim246@gmail.com>
2025-01-14 02:37:14 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './components/calculator';
+
+function App() {
+  return (
+    <div className='App'> 
+      <Calculator />
+
+      <p className='Developer'>Developed By <span>Conan</span></p>
+    </div>
+    
+  )
+}
+
+export default App
